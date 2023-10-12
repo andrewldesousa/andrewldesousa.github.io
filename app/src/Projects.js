4b900635f682717ca0c32c3d7363486b3bae6b14
Andrew Desousa <andrew@andrews-mbp.lan>
2023-10-11 18:10:37 -0700

    site refresh, add routing

--- app/src/Projects.js
+++ app/src/Projects.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Outlet, Link } from "react-router-dom";
 
 
-function App() {
+function Projects() {
   return (
     <div className="page-container">
       <main>
@@ -13,14 +13,9 @@ function App() {
         </div>
         <div className="home-grid">
           <div>
-            <h2>About</h2>
-            <p>
-              I'm a software engineer based in San Francisco, California.
-              In 2022, I graduated from the Technical Unviersity of Munich with a Master's degree in Computer Science where I focused on Machine Learning and Computer Vision. Prior to that I completed a Bachelor's degree in Computer Science at the University of Miami.
-              <br></br><br></br>
-              I'm currently interested in the research and applications of Machine Learning and Computer Vision (NeRFs, Deep Generative Modeling, Diffusion Models, and others). I am also interested in Backend Engineering, System Design, and Machine Learning Engineering.
-            </p>
-          </div>          
+            <h2>Projects</h2>
+            <p style={{width:"700px"}}>Coming soon...</p> {/* This is a hack */}
+          </div>
         </div>
       </main>
 
@@ -41,4 +36,4 @@ function App() {
   );
 }
 
-export default App;
+export default Projects;
--- app/src/index.js
+++ app/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Projects from './Projects';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 reportWebVitals();
