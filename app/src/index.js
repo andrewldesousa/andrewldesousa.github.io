f9aee9299216bb5d54278f2304c63cadfff8ebc0
Andrew Desousa <andrew@andrews-mbp.lan>
2023-10-15 10:22:19 -0700

    use HashRouter

--- app/src/index.js
+++ app/src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import DiffusionModels from './pages/Diffusion';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route index element={<Home />} />
-      <Route path="*" element={<Home />} />
       <Route path="/diffusion" element={<DiffusionModels />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
